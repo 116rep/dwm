444509aa0c73e292eece9d5bd8562c7d0563f19b
116rep <samuellalley@gmail.com>
2025-03-08 21:03:53 -0800

    changed the color bar

--- config.h
+++ config.h
@@ -7,15 +7,22 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray1[]       = "#26233a";
+static const char col_gray2[]       = "#f2e9e1";
+static const char col_gray3[]       = "#393552";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char col_cyan[]        = "#393552";
+//static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//};
+
+/* Rosé Pine color scheme with Overlay emphasis */
+static const char *colors[][3]      = {
+    /*               fg         bg         border   */
+    [SchemeNorm] = { "#e0def4", "#26233a", "#1f1d2e" }, /* Normal (unfocused) */
+    [SchemeSel]  = { "#26233a", "#e0def4", "#eb6f92" }, /* Selected (focused) */
 };
 
 /* tagging */
@@ -71,7 +78,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
